6acd6fb0e71b391f47d756da48d4858fe9f022e7
Josh Lee <joshua.huggins@sbcglobal.net>
2024-02-26 20:25:24 -0800

    added the function to check current time against timeblock to determine style class

--- Develop/script.js
+++ Develop/script.js
@@ -3,18 +3,59 @@
 var today = dayjs();
 $('#currentDay').text(today.format('dddd, MMMM D'));
 
-var CurrentHour = dayjs().hour();
-var Timetable = 
-   console.log(CurrentHour);
-  if(CurrentHour >= 9 && CurrentHour <= 17) {
-    $('.description').setClass('present');
+// Need the document to be ready before running the functions for 
+// the "Past, Present, Future" functions for formatting the time blocks
+$(document).ready(function () {
+//this will be the time blocks function
+  function timeBlock() {
+    var CurrentHour = dayjs().hour();
+    console.log(CurrentHour);
+
+    // The loop to go through each block.
+    $(".time-block").each(function () {
+      var block = parseInt($(this).attr("id").split("hour")[1]);
+      console.log(block);
+      // Conditionals that will check time and add classes based off the 
+      // var block to get the info needed for the conditional
+      if (block < CurrentHour) {
+        // This one will add the class "past" to any hours that have happened
+        // for the day.
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (block === CurrentHour) {
+        // This will make the current hour a red time block to indicate
+        // current hour on tthe timetable
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+      else {
+        // If no other conditionals are met they should be considered 
+        // future hours, they should be shown as green on the timetable
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    })
   }
-  else if(CurrentHour < 9) {
-    $('.description').setClass('past');
-  }
-  else {
-    $('.description').addClass('future');
-  };
+  // This will grab the info needed to compare current time to 
+  // time blocks listed. Dont use "-" between the hour and number
+  // will throw a negative number and cause issues with the conditionals.
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+  // calls on the function to set the formats
+  timeBlock();
+});
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
